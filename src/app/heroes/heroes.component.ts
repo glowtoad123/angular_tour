0858516ece7ecce241424267c127929d850bad83
Alonzo Austin <alonzo6546@outlook.com>
2020-09-02 12:14:24 -0400

    learned routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,25 +15,9 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   }; */
 
-  selectedHero: Hero;
-
+  /* selectedHero: Hero;
+ */
   heroes: Hero[];
-
-/* Without observable
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes()
-  } */
-
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => 
-      this.heroes = heroes
-    )
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
 
   constructor(
     private heroService: HeroService,
@@ -45,5 +29,21 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
     this.getHeroes()
   }
+
+  /* Without observable
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes()
+  } */
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => 
+      this.heroes = heroes
+    )
+  }
+
+  /* onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  } */
 
 }
